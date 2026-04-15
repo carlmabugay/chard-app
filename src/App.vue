0a2a70e2015f6572cd4a7fc7a9ead4b9a8034924
Carl Anthony Mabugay <mabugay.carl@gmail.com>
2026-04-15 22:05:18 +0800

    init: docker, vue, and tailwindcss setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-screen flex items-center justify-center bg-black">
+    <h1 class="text-4xl font-bold text-lime-300">
+      Hi Mom!
+    </h1>
+  </div>
+</template>
